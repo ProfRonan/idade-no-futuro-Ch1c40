100326e7027ad2a8dd7e256d087812e006834a42
Francisco Arley Coelho Barros <107206805+Ch1c40@users.noreply.github.com>
2023-03-25 16:22:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ nome = input()
 
 soma = (ano2 - ano1) + idade
 
-print(nome, "\n, no ano de ", ano2, " você terá ", soma, " anos")
+print("{}, no ano de {} você terá {} anos", .format(nome, ano2, soma))
