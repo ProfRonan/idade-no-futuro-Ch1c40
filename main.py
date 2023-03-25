27abd7e5429d3ff56029868d52a4c50898741720
Francisco Arley Coelho Barros <107206805+Ch1c40@users.noreply.github.com>
2023-03-25 11:53:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ idade= int(input("Digite sua idade: "))
 ano2 = int(input("Digite um ano para prever sua idade: "))
 nome = str(input("Digite seu nome: "))
 
-ano3 = (ano2 - ano1)
-soma = ano3 + idade
+idade2 = (ano2 - ano1) + idade
 
-print(nome, "no ano de ", ano2, "você tera ", soma, " anos")
+print(f"{nome}, no ano de {ano2} você terá {idade2} anos")
